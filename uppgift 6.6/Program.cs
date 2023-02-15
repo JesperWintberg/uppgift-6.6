b378470284dd7ecd59d4c9ffaa6b57686d89880b
Pingvu <nhlpool27@gmail.com>
2023-02-15 12:00:24 +0100

    Add project files.

---
+++ uppgift 6.6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ord();
+        }
+
+        static void Ord()
+        {
+            Console.Write("skriv in en mening: ");
+            string mening = Console.ReadLine();
+
+            string[] mening2 = mening.Split(" ");
+
+            for (int i = 0; i < mening2.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    mening2[i] = mening2[i].ToUpper();
+
+                }
+
+            }
+            for (int j = 0; j < mening2.Length; j++)
+            {
+                Console.Write($" {mening2[j]}");
+            }
+        }
+
+    }
+}
+
